2898b2658e685207affb76b3a575565ea2619891
Philipp Nikitin <Nikitin.FS@yandex.ru>
2024-06-22 19:07:55 +0300

    task16 done

--- task16/task16.go
+++ task16/task16.go
@@ -1,17 +1,31 @@
 package main
 
-import (
-	"fmt"
-	"unsafe"
-)
+import "fmt"
+
+func quicksort(unsorted []int) []int {
+	if len(unsorted) <= 1 { // если слайс пустой или содержит один элемент - возвращаем данный слайс без изменений
+		return unsorted
+	} else if len(unsorted) == 2 {
+		if unsorted[0] > unsorted[1] { // если элемент с индексом 0 меньше, или равен элементу с индексом 1
+			unsorted[0], unsorted[1] = unsorted[1], unsorted[0] // обмениваем значения переменных
+		}
+		return unsorted
+	} // если слайс состоит из трех элементов или больше
+	pivot := unsorted[0]      // создаем опорный элемент, в качестве которого выбираем первый элемент слайса
+	less := []int{}           // создаем слайс, в котором будем хранить элементы меньше опорного
+	equalOrGreater := []int{} // создаем слайс, в котором будем хранить элементы больше опорного
+	for _, val := range unsorted[1:] {
+		if val < pivot { // если рассматриваемый элемент меньше опорного, необходимо перенести его в начало слайса
+			less = append(less, val)
+		} else { // if val >= support
+			equalOrGreater = append(equalOrGreater, val)
+		}
+	}
+	return append(append(quicksort(less), []int{pivot}...), quicksort(equalOrGreater)...)
+}
 
 func main() {
-	var s struct{}
-	fmt.Println(unsafe.Sizeof(s))
-	var i interface{}
-	fmt.Println(unsafe.Sizeof(i))
-	var b bool
-	fmt.Println(unsafe.Sizeof(b))
-	var str string
-	fmt.Println(unsafe.Sizeof(str))
+	unsorted := []int{8, 7, 19, 0, -3, 547, -900, 2345, -9, 0, 5} // попробовать работу функции с пустым слайсом или со слайсом, состоящим из одного элемента
+	sorted := quicksort(unsorted)
+	fmt.Println(sorted)
 }
